edebc80453cb3ee9f6d8a5052969d3b1a019e439
Thanoyan Premananthan <premananthanthanoyan@gmail.com>
2026-04-15 22:53:19 +0530

    Answer.js file was added

---
+++ Answer.js
@@ -0,0 +1,87 @@
+// Q1:
+var studentName="Thanoyan";
+var age=20;
+
+console.log(studentName,age)
+
+// Q2:
+let length = 10;
+let width = 5;
+
+let area = length * width;
+
+console.log(area);
+
+// Q3:
+let bookName = "The Great Gatsby";
+let pagesPerDay = 25;
+let days = 4;
+
+let totalPagesRead = pagesPerDay * days;
+
+console.log(totalPagesRead);
+
+// Q4: 
+let distance = 150; 
+let time = 3;      
+
+let speed = distance / time;
+
+console.log(speed);
+
+// Q5:
+let celsius = 25;
+
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log("Celsius:", celsius);
+console.log("Fahrenheit:", fahrenheit);
+
+// Q6: 
+let subject1 = 85;
+let subject2 = 90;
+let subject3 = 78;
+
+let totalMarks = subject1 + subject2 + subject3;
+
+let average = totalMarks / 3;
+
+console.log("Total Marks: " + totalMarks);
+console.log("Average: " + average.toFixed(2));
+
+// Q7:
+let productName = "Laptop";
+let price = 1200;
+let tax = 180;
+
+let finalPrice = price + tax;
+
+
+console.log(`Product: ${productName}`);
+console.log(`Final Price: $${finalPrice}`);
+
+// Q8:
+let base = 10;
+let height = 5;
+
+
+let Area = (base * height) / 2;
+
+console.log("The area of the triangle is: " + Area);
+
+// Q9: 
+let minutes = 5;
+
+let seconds = minutes * 60;
+
+console.log(minutes + " minutes is equal to " + seconds + " seconds.");
+
+// Q10:
+let number1 = 20;
+let number2 = 5;
+
+let multiplication = number1 * number2;
+let division = number1 / number2;
+
+console.log("Multiplication Result: " + multiplication);
+console.log("Division Result: " + division);
